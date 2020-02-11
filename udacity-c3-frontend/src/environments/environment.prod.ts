9505c02f06e4986ad0175ed361862f8c7a195a54
Nitesh Sharma <nitesh.grep@gmail.com>
2020-02-11 13:59:09 +0530

    Adding docker and deployment scripts

--- udacity-c3-frontend/src/environments/environment.prod.ts
+++ udacity-c3-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ec2-15-206-178-112.ap-south-1.compute.amazonaws.com:8080/api/v0/feed'
 };
 
 /*
